6de2ed76fabb0dcf8fd3c6b1fbbfe469b78933ad
shiki <kamiacgxu@gmail.com>
2025-06-10 13:57:44 +0800

    feat: add list

--- src/pages/torrent2magnet.vue
+++ src/pages/torrent2magnet.vue
@@ -22,18 +22,42 @@
     </v-row>
 
     <v-row v-if="selectedFiles && selectedFiles.length">
+      <v-col :cols="5">
+        <v-list>
+          <v-list-item v-for="(file, index) in selectedFiles" :key="index">
+            <template #prepend>
+              <v-btn
+                :border="false"
+                class="btn-mgr"
+                icon="mdi-close"
+                size="x-small"
+                @click="removeFile(index)"
+              />
+            </template>
+            <div>
+              <v-list-item-title :title="file.name">{{ file.name }}</v-list-item-title>
+              <v-list-item-subtitle>{{ formatFileSize(file.size) }}</v-list-item-subtitle>
+            </div>
+          </v-list-item>
+        </v-list>
+      </v-col>
+      <v-col :cols="1">123</v-col>
       <v-col :cols="6">
         <v-list>
           <v-list-item v-for="(file, index) in selectedFiles" :key="index">
+            <template #prepend>
+              <v-btn
+                :border="false"
+                class="btn-mgr"
+                icon="mdi-content-copy"
+                size="x-small"
+                @click="handleCopy(index)"
+              />
+            </template>
             <div>
-              <v-list-item-title>{{ file.name }}</v-list-item-title>
+              <v-list-item-title :title="file.name">{{ file.name }}</v-list-item-title>
               <v-list-item-subtitle>{{ formatFileSize(file.size) }}</v-list-item-subtitle>
             </div>
-            <v-list-item-action>
-              <v-btn icon @click="removeFile(index)">
-                <v-icon>mdi-close</v-icon>
-              </v-btn>
-            </v-list-item-action>
           </v-list-item>
         </v-list>
       </v-col>
@@ -49,7 +73,7 @@
   const emit = defineEmits(['files-selected'])
   const fileInput = ref<InstanceType<typeof VFileInput> | null>(null)
 
-  const selectedFiles = ref([])
+  const selectedFiles = ref<File[]>([])
 
   const triggerFileInput = () => {
     fileInput.value?.$el.querySelector('input[type="file"]').click()
@@ -73,6 +97,20 @@
     const i = Math.floor(Math.log(bytes) / Math.log(k))
     return `${(bytes / Math.pow(k, i)).toFixed(2)} ${sizes[i]}`
   }
+
+  const handleCopy = (index: number) => {
+    // 复制文件名到剪贴板
+    const file = selectedFiles.value[index]
+    if (file) {
+      navigator.clipboard.writeText(file.name)
+        .then(() => {
+          console.log('文件名已复制到剪贴板')
+        })
+        .catch(err => {
+          console.error('复制文件名到剪贴板时出错:', err)
+        })
+    }
+  }
 </script>
 
 
@@ -86,6 +124,10 @@
     font-size: 20px;
     font-weight: bold;
     margin-bottom: 20px;
+  }
+
+  .btn-mgr {
+    margin-right: 10px;
   }
 }
 </style>
